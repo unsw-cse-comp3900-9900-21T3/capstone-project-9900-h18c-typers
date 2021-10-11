35c5891adb1258ca8fe1de6ce0100e8181c8b68c
Runsen Zhang <runsen.zhang@student.unsw.edu.au>
2021-10-11 20:56:41 +0800

    Check userexist interface Check mobileexist interface

--- Dpay/Dpay/apps/users/urls.py
+++ Dpay/Dpay/apps/users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+import re
 
 urlpatterns = [
-    path('register/', views.RegisterView.as_view(), name='home')
+    path('register/', views.RegisterView.as_view()),
+    path(r'usernames/<username>/count/', views.CheckUsernameView.as_view()),
+    path(r'mobiles/<int:mobile>/count/', views.CheckMobileView.as_view())
 ]
--- Dpay/Dpay/apps/users/views.py
+++ Dpay/Dpay/apps/users/views.py
@@ -15,7 +15,7 @@ from users.models import User
 class RegisterView(View):
     @method_decorator(csrf_exempt)
     def post(self, request):
-        # 1, 获取参数
+
         user_name = request.POST.get("user_name")
         pwd = request.POST.get("pwd")
         cpwd = request.POST.get("cpwd")
@@ -23,28 +23,37 @@ class RegisterView(View):
         msg_code = request.POST.get("msg_code")
         allow = request.POST.get("allow")
 
-        # 2, 校验参数
-        # 2,1 为空校验
         if not all([user_name, pwd, cpwd, phone, msg_code, allow]):
             return http.HttpResponseForbidden("参数不全")
 
-        # 2,2 两次密码一致
         if pwd != cpwd:
             return http.HttpResponseForbidden("两次密码不一致")
 
-        # 2,3 手机号格式正确
         if not re.match(r'^1[3-9]\d{9}$', phone):
             return http.HttpResponseForbidden("手机号格式有误")
 
-        # 2,5 短信验证码正确(下一次完成)
         print("msg_code = %s" % msg_code)
 
-        # 2,6 协议需要同意
         if allow != 'on':
             return http.HttpResponseForbidden("协议需要同意")
 
-        # 3, 数据入库
         user = User.objects.create(username=user_name, password=pwd, mobile=phone)
 
-        # 4, 返回响应
         return JsonResponse({'succ':1},safe=False)
+
+class CheckUsernameView(View):
+        def get(self, request, username):
+            # 1, 根据用户名查询用户数量
+            count = User.objects.filter(username=username).count()
+            print(count)
+            # 2, 返回响应
+            return http.JsonResponse({"count": count, "code": 0})
+
+
+class CheckMobileView(View):
+    def get(self, request, mobile):
+        # 1, 根据用户名查询用户数量
+        count = User.objects.filter(mobile=mobile).count()
+        print(count)
+        # 2, 返回响应
+        return http.JsonResponse({"count": count, "code": 0})
